b960bcb3908a545597887b8a1e9ecff6ea3a1623
elisa33 <pipi.tdfuego@gmail.com>
2021-08-04 18:32:45 -0400

    noticia single

--- src/components/New/index.jsx
+++ src/components/New/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Markup } from 'interweave';
+import React,{useState, useEffect} from 'react'
 import {Markupi, Wrapper} from './styled.new'
 
-const New = ({ noticias}) => {
+const New = ({ }) => {
 
-    const {titulo, bajada, fecha, contenido} = noticias
-   
+    const [ nueva, setNueva] = useState([])
+
+    const getNew = async ()=>{
+        const noticia = await fetch('https://wp.ojosdecafe.com/?rest_route=/gs/v1/noticia/del-norte-de-suecia-al-sur-de-mendoza-abisko-tabaco-munchies')
+        const newprincipal = await noticia.json()
+        setNueva(newprincipal)
+    } 
+
+    useEffect(()=>{
+        getNew()
+    },[])
+
+    const {titulo, bajada, fecha, contenido} = nueva   
 
     return (
         <div>
             <Wrapper>
                 <h2>{titulo}</h2>
-                <h4>{bajada}</h4>
+                <h5>{bajada}</h5>
                 <span>{fecha}</span>
                 <Markupi content={contenido} />
             </Wrapper>

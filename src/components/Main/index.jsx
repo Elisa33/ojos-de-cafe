696ee49939557dadcec3297d6a0b0846fdba818f
elisa33 <pipi.tdfuego@gmail.com>
2021-08-04 18:32:00 -0400

    fetch news

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,37 +1,48 @@
 import React,{useEffect, useState} from 'react'
 import { Container } from './styled.main'
-import New from '../New/index'
+import Locales from '../Locales/Locales'
+import Principal from './../Principal/index';
+import New from './../New/index';
 
 const Main = () => {
 
-    const [noticias, setNoticias] = useState(null) 
-
-    useEffect(()=>{
-        getData()
-        getDataAll()
-    },[])
-
-    const getData = async ()=>{
-        const data = await fetch('https://wp.ojosdecafe.com/?rest_route=/gs/v1/noticia/del-norte-de-suecia-al-sur-de-mendoza-abisko-tabaco-munchies')
-        const notis = await data.json()
-        setNoticias(notis)
-
-    }
-
-    const [all, setAll] = useState(null) 
+    const [all, setAll] = useState([]) 
 
     const getDataAll = async ()=>{
+        const datos = await fetch('https://wp.ojosdecafe.com/?rest_route=/gs/v1/home',  {
+        "method": "GET",
+        "headers": {}
+    })
+        const todas = await datos.json()
+        setAll(todas)
+    }
+
+    
+    
+    useEffect(()=>{
+        getDataAll()
+    },[])
+    
+    const principal = all.principal;
+    //console.log(principal)
+    const locales = all.locales
+    console.log(locales)
+
+
+    /* const getDataAll = async ()=>{
         const datos = await fetch('https://wp.ojosdecafe.com/?rest_route=/gs/v1/home')
         const todas = await datos.json()
         setAll(todas)
-        console.log(all)
+    } */
 
-    }
 
     return (
         <Container>
-            <New noticias={noticias}/>
-            <h2></h2>
+
+            
+            <Principal principal={principal}/>
+            <Locales locales={locales}/>
+            <New/>
         </Container>
     )
 }
--- src/components/Main/styled.main.jsx
+++ src/components/Main/styled.main.jsx
@@ -2,6 +2,6 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     max-width: 1280px;
-    margin: 1rem auto;
+    margin: 2rem auto;
     color: #666;
 `

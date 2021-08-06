d360dfed7357bab6201d2d22f5f0fc88d196daf8
elisa33 <pipi.tdfuego@gmail.com>
2021-08-05 20:35:01 -0400

    fix problem with props

--- src/components/Locales/Locales.jsx
+++ src/components/Locales/Locales.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import { Card, Wrapper, Grid} from './styled.locales';
 
-const Locales = ({locales}) => {
-    //no me toma el componetne locales con las props...
-    const { titulo, imagen_destacada } = locales[0];
+const Locales = (props) => {
+   const { locales } = props;
     return (
         <Wrapper>
             <Grid>
                 { locales.map(item =>
-                    <Card>
+                    (<Card key={item.id}>
                         <img src={item.imagen_destacada} alt="" />
                         <h2>{item.titulo}</h2>
-                    </Card>
+                    </Card>)
                 )}
 
             </Grid>

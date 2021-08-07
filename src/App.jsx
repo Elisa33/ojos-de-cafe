32ca12d1138fe8faa3f37ce9418f27e947189363
elisa33 <pipi.tdfuego@gmail.com>
2021-08-06 20:03:36 -0400

    clean app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 import React, { useState } from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
 import Main from './components/Main'
-import Footer from './components/Footer'
-
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <Navbar>
-        
-      </Navbar>
       <Main/>
-      <Footer/>
     </div>
   )
 }
